b3a79beee6b0fff9ae7fd55e1a5a0b38e0c790b3
Viraj Sinha <root@vsinha.com>
2022-09-22 08:24:48 -0700

    update link script

--- linkConfigFiles.sh
+++ linkConfigFiles.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "linking bashrc, vimrc, tmux.conf"
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s $(HOME)/dotfiles/bashrc ~/.bashrc
+ln -s $(HOME)/dotfiles/vimrc ~/.vimrc
+ln -s $(HOME)/dotfiles/tmux.conf ~/.tmux.conf
+ln -s $(HOME)/dotfiles/zshrc ~/.zshrc
+ln -s $(HOME)/dotfiles/gitconfig ~/.gitconfig
+ln -s $(HOME)/dotfiles/doom.d ~/.doom.d
+ln -s $(HOME)/dotfiles/nvim ~/.config/nvim
